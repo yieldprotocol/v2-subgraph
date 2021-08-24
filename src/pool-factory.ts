2a103aca0aa86581965aa7dbe3945fefad92ae0e
David Mihal <david@dmihal.com>
2021-08-24 14:26:43 +0100

    Create basic subgraph for tracking pools

---
+++ src/pool-factory.ts
@@ -0,0 +1,12 @@
+import { PoolFactory, PoolCreated } from "../generated/PoolFactory/PoolFactory"
+import { Pool } from "../generated/schema"
+import { Pool as PoolTemplate } from '../generated/templates'
+
+export function handlePoolCreated(event: PoolCreated): void {
+  let pool = new Pool(event.params.pool.toHexString())
+  pool.base = event.params.base
+  pool.fyToken = event.params.fyToken
+  pool.save()
+
+  PoolTemplate.create(event.params.pool)
+}
---
+++ src/pool.ts
@@ -0,0 +1,3 @@
+import { Pool, Trade as TradeEvent } from "../generated/templates/Pool/Pool"
+
+export function handleTrade(event: TradeEvent): void {}
