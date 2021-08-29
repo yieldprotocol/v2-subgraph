e64f5bf9ca1b142fb0eb2f07b60959b584ddba59
David Mihal <david@dmihal.com>
2021-08-30 00:14:20 +0100

    Connect pool to fytoken

--- src/pool-factory.ts
+++ src/pool-factory.ts
@@ -5,7 +5,7 @@ import { Pool as PoolTemplate } from '../generated/templates'
 export function handlePoolCreated(event: PoolCreated): void {
   let pool = new Pool(event.params.pool.toHexString())
   pool.base = event.params.base
-  pool.fyToken = event.params.fyToken
+  pool.fyToken = event.params.fyToken.toHexString()
   pool.save()
 
   PoolTemplate.create(event.params.pool)
