503a507132c851fee9da67bbd8897979ff9630ee
David Mihal <david@dmihal.com>
2021-08-31 12:22:59 +0100

    Track pool reserves

--- src/pool-factory.ts
+++ src/pool-factory.ts
@@ -1,11 +1,14 @@
 import { PoolFactory, PoolCreated } from "../generated/PoolFactory/PoolFactory"
 import { Pool } from "../generated/schema"
 import { Pool as PoolTemplate } from '../generated/templates'
+import { ZERO } from "./lib"
 
 export function handlePoolCreated(event: PoolCreated): void {
   let pool = new Pool(event.params.pool.toHexString())
   pool.base = event.params.base
   pool.fyToken = event.params.fyToken.toHexString()
+  pool.fyTokenReserves = ZERO.toBigDecimal()
+  pool.baseReserves = ZERO.toBigDecimal()
   pool.save()
 
   PoolTemplate.create(event.params.pool)
--- src/pool.ts
+++ src/pool.ts
@@ -1,3 +1,21 @@
-import { Pool, Trade as TradeEvent } from "../generated/templates/Pool/Pool"
+import { Pool, Trade as TradeEvent, Liquidity as LiquidityEvent } from "../generated/templates/Pool/Pool"
+import { Pool } from "../generated/schema"
+import { EIGHTEEN_DECIMALS } from "./lib"
 
-export function handleTrade(event: TradeEvent): void {}
+export function handleTrade(event: TradeEvent): void {
+  let pool = Pool.load(event.address.toHexString())
+
+  pool.fyTokenReserves -= event.params.fyTokens.divDecimal(EIGHTEEN_DECIMALS)
+  pool.baseReserves -= event.params.bases.divDecimal(EIGHTEEN_DECIMALS)
+
+  pool.save()
+}
+
+export function handleLiquity(event: LiquidityEvent): void {
+  let pool = Pool.load(event.address.toHexString())
+
+  pool.fyTokenReserves -= event.params.fyTokens.divDecimal(EIGHTEEN_DECIMALS)
+  pool.baseReserves -= event.params.bases.divDecimal(EIGHTEEN_DECIMALS)
+
+  pool.save()
+}
