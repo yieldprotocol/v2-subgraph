a3445bc1a76f4f6cee0cda39087f51b420b2aa5b
marcomariscal <marco.a.mariscal@gmail.com>
2022-05-25 12:27:10 -0400

    feat: add witch address

--- src/cauldron.ts
+++ src/cauldron.ts
@@ -1,18 +1,42 @@
-import { store, Address, Bytes, ethereum } from '@graphprotocol/graph-ts'
 import {
-  Cauldron, AssetAdded, SeriesAdded, IlkAdded, VaultBuilt, VaultTweaked, VaultPoured, VaultStirred, VaultRolled, SeriesMatured
-} from "../generated/Cauldron/Cauldron"
-import { IERC20 } from "../generated/Cauldron/IERC20"
-import { Asset, Collateral, SeriesEntity, Vault, FYToken } from "../generated/schema"
-import { EIGHTEEN_DECIMALS, ZERO, toDecimal } from './lib'
+  store,
+  Address,
+  Bytes,
+  ethereum,
+  dataSource,
+} from "@graphprotocol/graph-ts";
+import {
+  Cauldron,
+  AssetAdded,
+  SeriesAdded,
+  IlkAdded,
+  VaultBuilt,
+  VaultTweaked,
+  VaultPoured,
+  VaultStirred,
+  VaultRolled,
+  SeriesMatured,
+} from "../generated/Cauldron/Cauldron";
+import { IERC20 } from "../generated/Cauldron/IERC20";
+import {
+  Asset,
+  Collateral,
+  SeriesEntity,
+  Vault,
+  FYToken,
+} from "../generated/schema";
+import yieldEnv from "../yieldEnv";
+import { EIGHTEEN_DECIMALS, ZERO, toDecimal } from "./lib";
+
+const WITCH = yieldEnv[dataSource.network()];
 
 function assetIdToAddress(cauldronAddress: Address, id: Bytes): Address {
-  let cauldron = Cauldron.bind(cauldronAddress)
-  return cauldron.assets(id)
+  let cauldron = Cauldron.bind(cauldronAddress);
+  return cauldron.assets(id);
 }
 
 function collateralId(seriesId: Bytes, ilkId: Bytes): string {
-  return seriesId.toHexString() + '-' + ilkId.toHexString()
+  return seriesId.toHexString() + "-" + ilkId.toHexString();
 }
 
 function tryOr(result: ethereum.CallResult<string>, fallback: string): string {
@@ -23,113 +47,136 @@ function tryOrInt(result: ethereum.CallResult<i32>, fallback: i32): i32 {
 }
 
 export function handleAssetAdded(event: AssetAdded): void {
-  let tokenContract = IERC20.bind(event.params.asset)
+  let tokenContract = IERC20.bind(event.params.asset);
 
-  let asset = new Asset(event.params.asset.toHexString())
-  asset.assetId = event.params.assetId
-  asset.name = tryOr(tokenContract.try_name(), 'Unknown Asset')
-  asset.symbol = tryOr(tokenContract.try_symbol(), 'ASSET')
-  asset.decimals = tryOrInt(tokenContract.try_decimals(), 0)
+  let asset = new Asset(event.params.asset.toHexString());
+  asset.assetId = event.params.assetId;
+  asset.name = tryOr(tokenContract.try_name(), "Unknown Asset");
+  asset.symbol = tryOr(tokenContract.try_symbol(), "ASSET");
+  asset.decimals = tryOrInt(tokenContract.try_decimals(), 0);
 
-  asset.totalFYTokens = ZERO.toBigDecimal()
-  asset.totalCollateral = ZERO.toBigDecimal()
-  asset.totalDebt = ZERO.toBigDecimal()
-  asset.totalTradingVolume = ZERO.toBigDecimal()
-  asset.totalInPools = ZERO.toBigDecimal()
+  asset.totalFYTokens = ZERO.toBigDecimal();
+  asset.totalCollateral = ZERO.toBigDecimal();
+  asset.totalDebt = ZERO.toBigDecimal();
+  asset.totalTradingVolume = ZERO.toBigDecimal();
+  asset.totalInPools = ZERO.toBigDecimal();
 
-  asset.save()
+  asset.save();
 }
 
 export function handleSeriesAdded(event: SeriesAdded): void {
-  let series = new SeriesEntity(event.params.seriesId.toHexString())
-  series.baseAsset = assetIdToAddress(event.address, event.params.baseId).toHexString()
-  series.fyToken = event.params.fyToken.toHexString()
-  series.matured = false
+  let series = new SeriesEntity(event.params.seriesId.toHexString());
+  series.baseAsset = assetIdToAddress(
+    event.address,
+    event.params.baseId
+  ).toHexString();
+  series.fyToken = event.params.fyToken.toHexString();
+  series.matured = false;
 
-  let fyToken = FYToken.load(event.params.fyToken.toHexString())
-  series.maturity = fyToken ? fyToken.maturity : 0
+  let fyToken = FYToken.load(event.params.fyToken.toHexString());
+  series.maturity = fyToken ? fyToken.maturity : 0;
 
-  series.save()
+  series.save();
 }
 
 export function handleIlkAdded(event: IlkAdded): void {
-  let collateral = new Collateral(collateralId(event.params.seriesId, event.params.ilkId))
-  collateral.series = event.params.seriesId.toHexString()
-  collateral.asset = assetIdToAddress(event.address, event.params.ilkId).toHexString()
+  let collateral = new Collateral(
+    collateralId(event.params.seriesId, event.params.ilkId)
+  );
+  collateral.series = event.params.seriesId.toHexString();
+  collateral.asset = assetIdToAddress(
+    event.address,
+    event.params.ilkId
+  ).toHexString();
 
-  collateral.save()
+  collateral.save();
 }
 
 export function handleVaultBuilt(event: VaultBuilt): void {
-  let vault = new Vault(event.params.vaultId.toHexString())
-  vault.owner = event.params.owner
-  vault.series = event.params.seriesId.toHexString()
-  vault.collateral = collateralId(event.params.seriesId, event.params.ilkId)
-  vault.debtAmount = ZERO.toBigDecimal()
-  vault.collateralAmount = ZERO.toBigDecimal()
+  let vault = new Vault(event.params.vaultId.toHexString());
+  vault.owner = event.params.owner;
+  vault.series = event.params.seriesId.toHexString();
+  vault.collateral = collateralId(event.params.seriesId, event.params.ilkId);
+  vault.debtAmount = ZERO.toBigDecimal();
+  vault.collateralAmount = ZERO.toBigDecimal();
 
-  vault.save()
+  vault.save();
 }
 
 export function handleVaultTweaked(event: VaultTweaked): void {
-  let vault = Vault.load(event.params.vaultId.toHexString())
-  vault.collateral = collateralId(event.params.seriesId, event.params.ilkId)
+  let vault = Vault.load(event.params.vaultId.toHexString());
+  vault.collateral = collateralId(event.params.seriesId, event.params.ilkId);
 
-  vault.save()
+  vault.save();
 }
 
 export function handleVaultDestroyed(event: VaultBuilt): void {
-  store.remove('Vault', event.params.vaultId.toHexString())
+  store.remove("Vault", event.params.vaultId.toHexString());
 }
 
 export function handleVaultPoured(event: VaultPoured): void {
-  let vault = Vault.load(event.params.vaultId.toHexString())
-  let collateral = Collateral.load(vault.collateral)
-  let series = SeriesEntity.load(vault.series)
-  let seriesAsset = Asset.load(series.baseAsset)
-  let collateralAsset = Asset.load(collateral.asset)
+  let vault = Vault.load(event.params.vaultId.toHexString());
+  let collateral = Collateral.load(vault.collateral);
+  let series = SeriesEntity.load(vault.series);
+  let seriesAsset = Asset.load(series.baseAsset);
+  let collateralAsset = Asset.load(collateral.asset);
 
-  vault.debtAmount += toDecimal(event.params.art, seriesAsset.decimals)
-  vault.collateralAmount += toDecimal(event.params.ink, collateralAsset.decimals)
+  vault.debtAmount += toDecimal(event.params.art, seriesAsset.decimals);
+  vault.collateralAmount += toDecimal(
+    event.params.ink,
+    collateralAsset.decimals
+  );
 
-  seriesAsset.totalDebt += toDecimal(event.params.art, collateralAsset.decimals)
-  collateralAsset.totalCollateral += toDecimal(event.params.ink, collateralAsset.decimals)
+  seriesAsset.totalDebt += toDecimal(
+    event.params.art,
+    collateralAsset.decimals
+  );
+  collateralAsset.totalCollateral += toDecimal(
+    event.params.ink,
+    collateralAsset.decimals
+  );
 
-  vault.save()
-  seriesAsset.save()
-  collateralAsset.save()
+  vault.save();
+  seriesAsset.save();
+  collateralAsset.save();
 }
 
 export function handleVaultStirred(event: VaultStirred): void {
-  let fromVault = Vault.load(event.params.from.toHexString())
-  let toVault = Vault.load(event.params.to.toHexString())
+  let fromVault = Vault.load(event.params.from.toHexString());
+  let toVault = Vault.load(event.params.to.toHexString());
 
-  let collateral = Collateral.load(fromVault.collateral)
-  let series = SeriesEntity.load(fromVault.series)
-  let seriesAsset = Asset.load(series.baseAsset)
-  let collateralAsset = Asset.load(collateral.asset)
+  let collateral = Collateral.load(fromVault.collateral);
+  let series = SeriesEntity.load(fromVault.series);
+  let seriesAsset = Asset.load(series.baseAsset);
+  let collateralAsset = Asset.load(collateral.asset);
 
-  fromVault.debtAmount -= toDecimal(event.params.art, seriesAsset.decimals)
-  fromVault.collateralAmount -= toDecimal(event.params.ink, collateralAsset.decimals)
-  toVault.debtAmount += toDecimal(event.params.art, seriesAsset.decimals)
-  toVault.collateralAmount += toDecimal(event.params.ink, collateralAsset.decimals)
+  fromVault.debtAmount -= toDecimal(event.params.art, seriesAsset.decimals);
+  fromVault.collateralAmount -= toDecimal(
+    event.params.ink,
+    collateralAsset.decimals
+  );
+  toVault.debtAmount += toDecimal(event.params.art, seriesAsset.decimals);
+  toVault.collateralAmount += toDecimal(
+    event.params.ink,
+    collateralAsset.decimals
+  );
 
-  fromVault.save()
-  toVault.save()
+  fromVault.save();
+  toVault.save();
 }
 
 export function handleVaultRolled(event: VaultRolled): void {
-  let vault = Vault.load(event.params.vaultId.toHexString())
+  let vault = Vault.load(event.params.vaultId.toHexString());
 
   // TODO: we need to update the art, right?
-  vault.series = event.params.seriesId.toHexString()
+  vault.series = event.params.seriesId.toHexString();
 
-  vault.save()
+  vault.save();
 }
 
 export function handleSeriesMatured(event: SeriesMatured): void {
-  let series = SeriesEntity.load(event.params.seriesId.toHexString())
-  series.matured = true
+  let series = SeriesEntity.load(event.params.seriesId.toHexString());
+  series.matured = true;
 
-  series.save()
+  series.save();
 }
