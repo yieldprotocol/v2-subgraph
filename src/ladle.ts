70b57aaff7aab67a6e41c41612ce4e9e88234c66
marcomariscal <marco.a.mariscal@gmail.com>
2022-05-26 09:33:26 -0400

    fix: import spelling

--- src/ladle.ts
+++ src/ladle.ts
@@ -1,10 +1,10 @@
-import { PoolAdded } from "../generated/Laddle/Laddle";
+import { PoolAdded } from "../generated/Ladle/Ladle";
 import { Pool } from "../generated/schema";
 import { createPool } from "./pool-factory";
 
 export function handlePoolAdded(event: PoolAdded): void {
-  let pool = Pool.load(event.params.pool.toHex())
+  let pool = Pool.load(event.params.pool.toHex());
   if (!pool) {
-    createPool(event.params.pool)
+    createPool(event.params.pool);
   }
 }
