e33bfc9bc4ecd6b0aa44ae65db8af7575a99434b
marcomariscal <marco.a.mariscal@gmail.com>
2023-03-16 09:35:56 -0700

    feat: account balance

--- src/accounts.ts
+++ src/accounts.ts
@@ -1,12 +1,44 @@
-import { Address } from "@graphprotocol/graph-ts";
-import { Account } from "../generated/schema";
+import { Address, BigDecimal } from "@graphprotocol/graph-ts";
+import { Account, AccountBalance, Asset } from "../generated/schema";
+import { getOrCreateAsset } from "./cauldron";
+import { ZERO_BD } from "./lib";
 
 export function getOrCreateAccount(address: Address): Account {
-  let account = Account.load(address.toHex())
+  let account = Account.load(address.toHex());
   if (!account) {
-    account = new Account(address.toHex())
-    account.numTrades = 0
-    account.save()
+    account = new Account(address.toHex());
+    account.numTrades = 0;
+    account.save();
   }
-  return account!
+  return account!;
+}
+
+export function getOrCreateAccountBalance(
+  accountAddr: Address,
+  assetAddr: Address
+): AccountBalance {
+  let account = getOrCreateAccount(accountAddr);
+  let asset = getOrCreateAsset(assetAddr, assetAddr);
+  let entityId = account.id + "-" + asset.id;
+
+  let balance = AccountBalance.load(entityId);
+
+  if (!balance) {
+    balance = new AccountBalance(entityId);
+    balance.account = account.id;
+    balance.asset = asset.id;
+    balance.balance = ZERO_BD;
+  }
+
+  return balance;
+}
+
+export function updateAccountBalance(
+  accountAddr: Address,
+  assetAddr: Address,
+  balanceChange: BigDecimal
+): void {
+  let accountBalance = getOrCreateAccountBalance(accountAddr, assetAddr);
+  accountBalance.balance = accountBalance.balance.plus(balanceChange);
+  accountBalance.save();
 }
